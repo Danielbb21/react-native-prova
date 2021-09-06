e81b8d7f50dd013a64401a0bcd2bbee26750a4db
Daniel <danielbb2000@gmail.com>
2021-09-06 18:31:35 -0300

    added funcionality of chose more than one game

--- Components/FilterGame.tsx
+++ Components/FilterGame.tsx
@@ -1,33 +1,82 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { getGameData } from "../store/GameSlice";
 import { useAppDispatch, useAppSelector } from "../store/store-hooks";
 import { getUserInfo } from "../store/UserSlice";
 import FilterGameButtons from "./FilterGameButtons";
 import { colors } from "../utils";
-const {PRIMARY_COLOR} = colors;
+const { PRIMARY_COLOR } = colors;
+
 const FilterGame = () => {
   const token = useAppSelector((state) => state.user.token);
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user.info);
   const allGames = useAppSelector((state) => state.game.items);
+  const [gameFilters, setGameFilters] = useState<string[]>([]);
 
   useEffect(() => {
     dispatch(getGameData(token));
 
     dispatch(getUserInfo(token));
-  }, []);
-  console.log(user);
+  }, [token, dispatch]);
+  
 
+  const setFilterHandler = (type: string) => {
+    console.log("typeGame", type);
+    const gameFind = gameFilters.find((game) => game === type);
+    if (!gameFind) {
+      setGameFilters((previus) => {
+        const array = [...previus];
+        array.push(type);
+        return array;
+      });
+      return;
+    }
+    setGameFilters((previus) => {
+      const array = [...previus];
+      return array.filter((element) => element !== type);
+    });
+  };
   return (
-    <View style = {{ top: 100,marginLeft: 20}}>
-        <Text style = {{fontSize: 22,color: PRIMARY_COLOR, alignItems: 'center', fontWeight: 'bold', fontStyle: 'italic' }}>Recent Games</Text>
-        <Text style ={{fontSize: 17, fontStyle: 'italic', color: '#868686'}}>Filters</Text>
+    <View style={{ top: 100, marginLeft: 20 }}>
+      <Text
+        style={{
+          fontSize: 22,
+          color: PRIMARY_COLOR,
+          alignItems: "center",
+          fontWeight: "bold",
+          fontStyle: "italic",
+        }}
+      >
+        Recent Games
+      </Text>
+      <Text style={{ fontSize: 17, fontStyle: "italic", color: "#868686" }}>
+        Filters
+      </Text>
       <View style={styles.buttons}>
         {allGames.length > 0 &&
-          allGames.map((game) => (
-            <FilterGameButtons  key ={game.id} color={game.color} name={game.type} />
-          ))}
+          allGames.map((game) => {
+              if(gameFilters.find(g => g === game.type)){
+                return (
+                    <FilterGameButtons
+                      chose={setFilterHandler.bind(null, game.type)}
+                      key={game.id}
+                      color={game.color}
+                      name={game.type}
+                      isClicked={true}
+                    />
+                  );
+              }
+              return (
+                <FilterGameButtons
+                  chose={setFilterHandler.bind(null, game.type)}
+                  key={game.id}
+                  color={game.color}
+                  name={game.type}
+                  isClicked={false}
+                />
+              );
+          })}
       </View>
     </View>
   );
@@ -37,7 +86,6 @@ export default FilterGame;
 
 const styles = StyleSheet.create({
   buttons: {
-   
     flexDirection: "row",
     width: "100%",
     alignItems: "center",
--- Components/FilterGameButtons.tsx
+++ Components/FilterGameButtons.tsx
@@ -1,33 +1,55 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-interface ButtonProps{
-    color: string;
-    name: string;
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+interface ButtonProps {
+  color: string;
+  name: string;
+  isClicked: boolean;
+  chose: () => void;
 }
-const FilterGameButtons:React.FC<ButtonProps> = (props) => {
+const FilterGameButtons: React.FC<ButtonProps> = (props) => {
   return (
-    <View style= {{...styles.buttonWrapper, borderColor: props.color}}>
-      <Text style= {{...styles.text, color: props.color}}>{props.name}</Text>
-    </View>
+    <TouchableOpacity onPress = {props.chose}>
+      {props.isClicked ? (
+        <View
+          style={{
+            ...styles.buttonWrapper,
+            borderColor: props.color,
+            backgroundColor: props.color,
+          }}
+        >
+          <View style = {{flexDirection: 'row'}}>
+            <Text style={{ ...styles.text, color: "#fff" }}>
+              {props.name} 
+            </Text>
+            <Icon name="close" style = {{color:'#fff'}}/>
+          </View>
+        </View>
+      ) : (
+        <View style={{ ...styles.buttonWrapper, borderColor: props.color }}>
+          <Text style={{ ...styles.text, color: props.color }}>
+            {props.name}
+          </Text>
+        </View>
+      )}
+    </TouchableOpacity>
   );
 };
 
 export default FilterGameButtons;
 
 const styles = StyleSheet.create({
-    buttonWrapper:{
-        width: 101,
-        height: 30,
-        borderWidth: 3,
-        borderRadius: 15,
-        alignItems: 'center',
-        justifyContent: 'center',
-        
-    },
-    text:{
-        fontSize: 14,
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-
-    }
-})
+  buttonWrapper: {
+    width: 101,
+    height: 30,
+    borderWidth: 3,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+});
