1bed386716581195d9f5b06709cb3eaa1d9d911e
Daniel <danielbb2000@gmail.com>
2021-09-06 14:08:36 -0300

    Config axios

--- Pages/Authentication.tsx
+++ Pages/Authentication.tsx
@@ -1,6 +1,9 @@
 import { useNavigation } from "@react-navigation/core";
-import React from "react";
+import axios from "axios";
+
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { api } from "../api";
 import Footer from "../Components/Footer";
 import Logo from "../Components/Logo";
 import SignIn from "../Components/SignIn";
@@ -8,14 +11,38 @@ import { colors } from "../utils";
 const { BACKGOUND_COLOR } = colors;
 
 const Authentication: React.FC = () => {
-    
-    
+  const myHeaders = new Headers();
+
+  /* 
+    myHeaders.append('Content-Type', 'application/json'); 
+    since it's a get request you don't need to specify your content-type
+  */
+
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjQxLCJpYXQiOjE2MzA5NDgwNTV9.3cPVdDceGkiIcnw_DFib_KQfxNsoZT3aRSG5PPTLa78";
+
+  myHeaders.append("Authorization", `Bearer ${token}`);
+  useEffect(() => {
+  
+
+    api
+      .get("/user", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log("response", response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
-    <Logo title="Authentication">
-      <SignIn />
-    </Logo>
-    <Footer/>
+      <Logo title="Authentication">
+        <SignIn />
+      </Logo>
+      <Footer />
     </>
   );
 };
---
+++ api/index.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: 'http://172.20.128.1:3333',
+    timeout: 1000
+});
